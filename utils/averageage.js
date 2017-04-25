40da19c593fe8352441f25ae45ff7570cf057928
Damien Murphy <damienomurchu@gmail.com>
2017-04-25 19:26:36 +0100

    refactors averageage handler

--- utils/averageage.js
+++ utils/averageage.js
@@ -4,39 +4,63 @@ var moment = require('moment');
 const sampleData = require('../sample-data/rc2.json');
 const utils = require('../utils/data.js');
 
-exports.averageAge = function (startDate, endDate) {
+exports.averageAge = function (jiraData, startDate, endDate) {
+
+  //console.log('start: ' + startDate);
+  //console.log('end: ' + endDate);
 
   var dateRange = utils.datesInRange(startDate, endDate);
-  //console.log(dateRange);
+  //console.log('dateRange: ' + dateRange);
 
   // pre-process jira data to reduce
-  var issues = sampleData.map(issue => {
+  var issues = jiraData.map(issue => {
     var iss = {};
     iss.created = issue.Created.substring(0, 10);
     iss.resolved = utils.resolvedDate(issue);
     return iss;
   });
+  //console.log('issues: ' + issues);
 
   // get relevant issues at each date point in range
   var dataset = {};
   dateRange.forEach(date => {
 
+    //console.log('date: ' + date);
+
     // get all unresolved & open issues that exist on this date
     var relevantIssues = issues.filter(iss => {
-      return (iss.created <= date) && !utils.resolvedNow(iss, date);
+      //console.log('iss.created <= date :' + (iss.created <= date));
+      //console.log('!utils.resolvedNow(iss, date) : ' + !utils.resolvedNow(iss, date));
+      var relevant = ((iss.created <= date) && !(utils.resolvedNow(iss, date)));
+      //console.log('relevant: ' + relevant);
+      return relevant;
     });
+    //console.log('relevant issues: ' + relevantIssues);
     var numIssues = relevantIssues.length; // get number of issues
+    //console.log('numIssues: ' + numIssues);
 
     // get age of each issue & derive average age of issues
-    dataset[date] = relevantIssues.map(iss => {
-      var created = moment(iss.created).add(12, 'hours'); // hack as date 1 hour off
-      var current = moment(date).add(12, 'hours'); // hack as date 1 hour off
-      return current.diff(created, 'days');
-    }).reduce((a, b) => {
-      return a + b;
-    }) / numIssues;
+    if (relevantIssues && relevantIssues.length > 1) {
+      //console.log('date: ' + date);
+      dataset[date] = relevantIssues.map(iss => {
+        //console.log('iss.created: ' + iss.created);
+        var created = moment(iss.created).add(12, 'hours'); // hack as date 1 hour off
+        var current = moment(date).add(12, 'hours'); // hack as date 1 hour off
+        return current.diff(created, 'days');
+      }).reduce((a, b) => {
+        return a + b;
+      }) / numIssues;
+    } else if (relevantIssues && relevantIssues.length === 1) {
+      dataset[date] = relevantIssues.map(iss => {
+        var created = moment(iss.created).add(12, 'hours'); // hack as date 1 hour off
+        var current = moment(date).add(12, 'hours'); // hack as date 1 hour off
+        return current.diff(created, 'days');
+      }) / numIssues;
+    } else {
+      dataset[date] = 0;
+    }
   });
   return dataset;
 };
 
-console.log(this.averageAge('2017-03-01', '2017-03-31'));
+//console.log(this.averageAge(sampleData, '2017-03-01', '2017-03-31'));
