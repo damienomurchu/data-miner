c25e6714c9f68611cd673fb6f001e63d6996a255
Damien Murphy <damienomurchu@gmail.com>
2017-04-25 19:28:00 +0100

    refactors sprint report handler

---
+++ utils/sprintReport.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const burndown = require('./burndown');
+const sampleData = require('../sample-data/rc2.json');
+
+var sprintName = 'IR300 Umbrella';
+
+exports.sprintReportData = function(jiraData, sprintName) {
+  var dataSet = {};
+  dataSet.report = 'Sprint report';
+  dataSet.sprint = sprintName;
+  dataSet.actualBurndown = burndown.actualBurndown(jiraData, sprintName);
+  dataSet.expectedBurndown = burndown.theoreticalBurndownLine(jiraData, sprintName);
+  dataSet.issueList = burndown.issueList(jiraData, sprintName);
+  return dataSet;
+}
+
+console.log(this.sprintReportData(sampleData, sprintName));
