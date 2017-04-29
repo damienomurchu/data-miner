79aa736d0d6669517928b08e9795916fb736c5f0
Damien Murphy <damienomurchu@gmail.com>
2017-04-29 09:37:26 +0100

    adds test files and stubs for remaining tests

---
+++ tests/utils/burndownChart-spec.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const burndownChart = require('../../utils/burndownChart');
+const chai = require('chai');
+var expect = chai.expect;
+const assert = chai.assert;
+var testData = require('../../sample-data/test-data.json');
+//const testUtils = require('./test-utils');
+const fixtures = require('./test-fixtures.json');
+
+describe('BurndownChart tests', function () {
+
+  const sampleWeek = fixtures.sampleWeek;
+
+  const start = fixtures.startDate;
+  const end = fixtures.endDate;
+
+  const sprintName = fixtures.sprintName;
+  const sprintStart = fixtures.sprintStart;
+  const sprintEnd = fixtures.sprintEnd;
+
+  beforeEach(function () {
+    //
+  });
+
+  afterEach(function () {
+    //
+  });
+
+  it('', function () {
+
+  });
+
+  it('', function () {
+
+  });
+
+  it('', function () {
+
+  });
+
+  it('', function () {
+
+  });
+
+  it('', function () {
+
+  });
+
+  it('', function () {
+
+  });
+
+});
---
+++ tests/utils/velocity-spec.js
@@ -0,0 +1,54 @@
+'use strict';
+
+const velocity = require('../../utils/velocity');
+const chai = require('chai');
+var expect = chai.expect;
+const assert = chai.assert;
+var testData = require('../../sample-data/test-data.json');
+//const testUtils = require('./test-utils');
+const fixtures = require('./test-fixtures.json');
+
+describe('Velocity tests', function () {
+
+  const sampleWeek = fixtures.sampleWeek;
+
+  const start = fixtures.startDate;
+  const end = fixtures.endDate;
+
+  const sprintName = fixtures.sprintName;
+  const sprintStart = fixtures.sprintStart;
+  const sprintEnd = fixtures.sprintEnd;
+
+  beforeEach(function () {
+    //
+  });
+
+  afterEach(function () {
+    //
+  });
+
+  it('', function () {
+
+  });
+
+  it('', function () {
+
+  });
+
+  it('', function () {
+
+  });
+
+  it('', function () {
+
+  });
+
+  it('', function () {
+
+  });
+
+  it('', function () {
+
+  });
+
+});
