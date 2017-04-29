f112ac2b53195616e373d80e306af4a4e794c8fb
Damien Murphy <damienomurchu@gmail.com>
2017-04-29 10:35:49 +0100

    updates required tests for burndownChart and sprintReport

--- tests/utils/burndownChart-spec.js
+++ tests/utils/burndownChart-spec.js
@@ -27,27 +27,63 @@ describe('BurndownChart tests', function () {
     //
   });
 
-  it('', function () {
+  it('should return an error object if no jiraData is passed in', function () {
 
   });
 
-  it('', function () {
+  it('should return an error object if no sprint name is passed in', function () {
 
   });
 
-  it('', function () {
+  it('should return error object if no sprint data present in jiraData passed in', function () {
 
   });
 
-  it('', function () {
+  it('returned object should not contain a null or undefined value for the "sprint" key', function () {
 
   });
 
-  it('', function () {
+  it('returned object should not contain a null or undefined value for the "report" key', function () {
 
   });
 
-  it('', function () {
+  it('returned object should contain a key "actualBurndown" whose value is an array', function () {
+
+  });
+
+  it('"actualBurndown" array should objects of key-value pairs with keys for "date" and "points"', function () {
+
+  });
+
+  it('keys for objects in the "actualBurndown" array should not be null or undefined', function () {
+
+  });
+
+  it('values for "date" keys of objects in the "actualBurndown" array should be date strings', function () {
+
+  });
+
+  it('values for "points" keys of objects in the "actualBurndown" array should be numbers', function () {
+
+  });
+
+  it('returned object should contain a key "expectedBurndown" whose value is an array', function () {
+
+  });
+
+  it('"expectedBurndown" array should objects of key-value pairs with keys for "date" and "points"', function () {
+
+  });
+
+  it('keys for objects in the "expectedBurndown" array should not be null or undefined', function () {
+
+  });
+
+  it('values for "date" keys of objects in the "expectedBurndown" array should be date strings', function () {
+
+  });
+
+  it('values for "points" keys of objects in the "expectedBurndown" array should be numbers', function () {
 
   });
 
