b52507a5060119f2de9e662addf41a9d7026797c
Damien Murphy <damienomurchu@gmail.com>
2017-05-06 12:11:37 +0100

    wip trialling alternative gnuplot wrapper "gnuplot"

---
+++ gnuplot-poc.js
@@ -0,0 +1,44 @@
+'use strict';
+
+// small proof of concept around generating graphs from the data-miner cli through gnuplot
+
+var gnuplot = require('gnuplot');
+var api = require('./npm-service-methods');
+var testData = require('./test/utils/test-jira-data.json');
+var fixtures = require('./test/utils/test-fixtures.json');
+
+'use strict';
+
+/*gnuplot()
+    .set('term png')
+    .set('output "out.png"')
+    .set('title "Burndown Chart, Sprint X"')
+    .set('xrange [0:20]')
+    .set('yrange [0:10]')
+    .set('zeroaxis')
+    .plot('(x/4)**2, sin(x), 1/x')
+    .end();*/
+
+var bd = api.burndownReportData(testData, fixtures.sprintName);
+console.log(bd.actualBurndown);
+console.log(bd.expectedBurndown);
+
+var xmax = bd.actualBurndown.length;
+var ymax = bd.actualBurndown[0].points;
+var actual = bd.actualBurndown;
+
+var x = [[1, 1], [2, 2], [3, 3]];
+var y = [[1, 4], [2, 5], [3, 4]];
+
+gnuplot()
+    .set('term png large')
+    .set('output "out.png"')
+    .set('title "Burndown Chart, Sprint X"')
+    .set(`xrange [0:${xmax}]`)
+    .set(`yrange [0:60]`)
+    .set('zeroaxis')
+    .plot('"sample.dat" using 1:2 with lines, "sample.dat" using 1:3 with lines')
+    .end();
+
+
+
