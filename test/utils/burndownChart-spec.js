f18168cdd6f58c6f70417b9435b7d4f1136f0f03
damien <damienomurchu@gmail.com>
2018-05-16 22:39:30 +0100

    updates burndownChart-spec

--- test/utils/burndownChart-spec.js
+++ test/utils/burndownChart-spec.js
@@ -2,8 +2,8 @@
 
 const burndownChart = require('../../utils/burndownChart');
 const chai = require('chai');
-var expect = chai.expect;
-var testData = require('./test-jira-data.json');
+const expect = chai.expect;
+const testData = require('./test-jira-data.json');
 const fixtures = require('./test-fixtures.json');
 
 describe('BurndownChart test', function () {
@@ -19,40 +19,40 @@ describe('BurndownChart test', function () {
   });
 
   it('should return an error object if no jiraData is passed in', function () {
-    var bd = burndownChart.burndownReportData(sprintName);
+    const bd = burndownChart.burndownReportData(sprintName);
     expect(bd.error).to.exist;
   });
 
   it('should return error object if bad jiraData passed in', function () {
-    var bd = burndownChart.burndownReportData([{}], sprintName);
+    const bd = burndownChart.burndownReportData([{}], sprintName);
     expect(bd.error).to.exist;
   });
 
   it('should return an error object if no sprint name is passed in', function () {
-    var bd = burndownChart.burndownReportData(testData);
+    const bd = burndownChart.burndownReportData(testData);
     expect(bd.error).to.exist;
   });
 
   it('returned object should contain a key "sprint" with an actual value', function () {
-    var bd = burndownChart.burndownReportData(testData, sprintName);
+    const bd = burndownChart.burndownReportData(testData, sprintName);
     expect(bd.sprint).to.exist;
     expect(bd.sprint).to.be.ok;
   });
 
   it('returned object should contain a key "report" with an actual value', function () {
-    var bd = burndownChart.burndownReportData(testData, sprintName);
+    const bd = burndownChart.burndownReportData(testData, sprintName);
     expect(bd.report).to.exist;
     expect(bd.report).to.be.ok;
   });
 
   it('returned object should contain a key "actualBurndown" whose value is an array', function () {
-    var bd = burndownChart.burndownReportData(testData, sprintName);
+    const bd = burndownChart.burndownReportData(testData, sprintName);
     expect(bd.actualBurndown).to.exist;
     expect(bd.actualBurndown instanceof Array).to.be.true;
   });
 
   it('"actualBurndown" array should objects of key-value pairs with keys for "date" and "points"', function () {
-    var bd = burndownChart.burndownReportData(testData, sprintName);
+    const bd = burndownChart.burndownReportData(testData, sprintName);
     bd.actualBurndown.forEach(datapoint => {
       expect(datapoint.date).to.exist;
       expect(datapoint.points).to.exist;
@@ -60,34 +60,34 @@ describe('BurndownChart test', function () {
   });
 
   it('values for object keys in the "actualBurndown" array should not be null or undefined', function () {
-    var bd = burndownChart.burndownReportData(testData, sprintName);
+    const bd = burndownChart.burndownReportData(testData, sprintName);
     bd.actualBurndown.forEach(datapoint => {
       expect(datapoint.date).to.exist;
     });
   });
 
   it('values for "date" keys of objects in the "actualBurndown" array should be date strings', function () {
-    var bd = burndownChart.burndownReportData(testData, sprintName);
+    const bd = burndownChart.burndownReportData(testData, sprintName);
     bd.actualBurndown.forEach(datapoint => {
       expect(typeof datapoint.date == 'string' ).to.be.true;
     });
   });
 
   it('values for "points" keys of objects in the "actualBurndown" array should be numbers', function () {
-    var bd = burndownChart.burndownReportData(testData, sprintName);
+    const bd = burndownChart.burndownReportData(testData, sprintName);
     bd.actualBurndown.forEach(datapoint => {
       expect(typeof datapoint.points == 'number' ).to.be.true;
     });
   });
 
   it('returned object should contain a key "expectedBurndown" whose value is an array', function () {
-    var bd = burndownChart.burndownReportData(testData, sprintName);
+    const bd = burndownChart.burndownReportData(testData, sprintName);
     expect(bd.expectedBurndown).to.exist;
     expect(bd.expectedBurndown instanceof Array).to.be.true;
   });
 
   it('"expectedBurndown" array should objects of key-value pairs with keys for "date" and "points"', function () {
-    var bd = burndownChart.burndownReportData(testData, sprintName);
+    const bd = burndownChart.burndownReportData(testData, sprintName);
     bd.expectedBurndown.forEach(datapoint => {
       expect(datapoint.date).to.exist;
       expect(datapoint.points).to.exist;
@@ -95,21 +95,21 @@ describe('BurndownChart test', function () {
   });
 
   it('values for object keys in the "expectedBurndown" array should not be null or undefined', function () {
-    var bd = burndownChart.burndownReportData(testData, sprintName);
+    const bd = burndownChart.burndownReportData(testData, sprintName);
     bd.expectedBurndown.forEach(datapoint => {
       expect(datapoint.date).to.exist;
     });
   });
 
   it('values for "date" keys of objects in the "expectedBurndown" array should be date strings', function () {
-    var bd = burndownChart.burndownReportData(testData, sprintName);
+    const bd = burndownChart.burndownReportData(testData, sprintName);
     bd.expectedBurndown.forEach(datapoint => {
       expect(typeof datapoint.date == 'string' ).to.be.true;
     });
   });
 
   it('values for "points" keys of objects in the "expectedBurndown" array should be numbers', function () {
-    var bd = burndownChart.burndownReportData(testData, sprintName);
+    const bd = burndownChart.burndownReportData(testData, sprintName);
     bd.expectedBurndown.forEach(datapoint => {
       expect(typeof datapoint.points == 'number' ).to.be.true;
     });
