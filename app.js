6e1d7fb77f3dac4f7812c27b7d4b6e04c6b3ccb1
damien <damienomurchu@gmail.com>
2018-05-16 22:15:34 +0100

    updates app.js

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const app = require('./server');
 
-//var server_port = process.env.OPENSHIFT_NODEJS_PORT || process.argv[2] || 8080
-var server_port = 8080;
-var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+const server_port = process.env.OPENSHIFT_NODEJS_PORT || process.argv[2] || 8080
+const server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
  
 if (!module.parent) {
   app.listen(server_port, server_ip_address, function () {
