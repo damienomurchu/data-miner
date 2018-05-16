e88713c3070c2562ff86f547639b0b683307ee11
damien <damienomurchu@gmail.com>
2018-05-16 22:38:26 +0100

    updates average-spec.js

--- test/utils/averageage-spec.js
+++ test/utils/averageage-spec.js
@@ -2,8 +2,8 @@
 
 const averageage = require('../../utils/averageage');
 const chai = require('chai');
-var expect = chai.expect;
-var testData = require('./test-jira-data.json');
+const expect = chai.expect;
+const testData = require('./test-jira-data.json');
 const fixtures = require('./test-fixtures.json');
 
 describe('Average-Age test', function () {
@@ -21,47 +21,47 @@ describe('Average-Age test', function () {
   });
 
   it('should return error object if no jiraData passed in', function () {
-    var aa = averageage.averageAge(start, end);
+    const aa = averageage.averageAge(start, end);
     expect(aa.error).to.exist;
   });
 
   it('should return error object if bad jiraData passed in', function () {
-    var aa = averageage.averageAge([{}], start, end);
+    const aa = averageage.averageAge([{}], start, end);
     expect(aa.error).to.exist;
   });
 
   it('should return error object if no start date passed in', function () {
-    var aa = averageage.averageAge(testData, end);
+    const aa = averageage.averageAge(testData, end);
     expect(aa.error).to.exist;
   });
 
   it('should return error object if no end date passed in', function () {
-    var aa = averageage.averageAge(testData, start);
+    const aa = averageage.averageAge(testData, start);
     expect(aa.error).to.exist;
   });
 
   it('should handle a date that is passed in as a string', function () {
-    var aa = averageage.averageAge(testData, start, end);
+    const aa = averageage.averageAge(testData, start, end);
     expect(aa).to.exist;
   });
 
   it('should handle a date that is passed in as a Date object', function () {
-    var aa = averageage.averageAge(testData, new Date(start), new Date(end));
+    const aa = averageage.averageAge(testData, new Date(start), new Date(end));
     expect(aa).to.exist;
   });
 
   it('should return an object with a number of key-value pairs equal to the date range', function () {
-    var aa = averageage.averageAge(testData, start, end);
+    const aa = averageage.averageAge(testData, start, end);
     expect(Object.keys(aa).length).to.equal(7);
   });
 
   it('each key-value pair in the returned object should have a key for each date in the range', function () {
-    var aa = averageage.averageAge(testData, datesInWeek[0], datesInWeek[6]);
+    const aa = averageage.averageAge(testData, datesInWeek[0], datesInWeek[6]);
     expect(aa).to.have.all.keys(datesInWeek);
   });
 
   it('each key-value pair in the returned object should have values that are numbers', function () {
-    var aa = averageage.averageAge(testData, start, end);
+    const aa = averageage.averageAge(testData, start, end);
     const values = Object.keys(aa).map(key => aa[key]);
     values.forEach(value => {
       expect(typeof value == 'number').to.be.true;
