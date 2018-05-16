987fde51491b4ac2b2a985327f668f5dc6fa9495
damien <damienomurchu@gmail.com>
2018-05-16 22:43:15 +0100

    updates velocity-spec

--- test/utils/velocity-spec.js
+++ test/utils/velocity-spec.js
@@ -2,8 +2,8 @@
 
 const velocity = require('../../utils/velocity');
 const chai = require('chai');
-var expect = chai.expect;
-var testData = require('./test-jira-data.json');
+const expect = chai.expect;
+const testData = require('./test-jira-data.json');
 const fixtures = require('./test-fixtures.json');
 
 describe('Velocity test', function () {
@@ -17,22 +17,22 @@ describe('Velocity test', function () {
   });
 
   it('should return error object if no jiraData passed in', function () {
-    var vc = velocity.velocity();
+    const vc = velocity.velocity();
     expect(vc[0].error).to.exist;
   });
 
   it('should return error object if bad jiraData passed in', function () {
-    var vc = velocity.velocity([{}]);
+    const vc = velocity.velocity([{}]);
     expect(vc[0].error).to.exist;
   });
 
   it('should return an array of one or more objects', function () {
-    var vc = velocity.velocity(testData);
+    const vc = velocity.velocity(testData);
     expect(vc.length).to.be.at.least(1);
   });
 
   it('each key-value pair in the returned object should have keys for "sprint", "expected", and "actual"', function () {
-    var vc = velocity.velocity(testData);
+    const vc = velocity.velocity(testData);
     vc.forEach(obj => {
       expect(obj.sprint).to.exist;
       expect(obj.expected).to.exist;
@@ -41,7 +41,7 @@ describe('Velocity test', function () {
   });
 
   it('each key-value pair in the returned object should have values that are numbers', function () {
-    var vc = velocity.velocity(testData);
+    const vc = velocity.velocity(testData);
     vc.forEach(obj => {
       expect(typeof obj.sprint == 'string' ).to.be.true;
       expect(typeof obj.expected == 'number' ).to.be.true;
